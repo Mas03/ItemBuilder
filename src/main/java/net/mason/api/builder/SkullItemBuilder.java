ff9d100436adf6b09f5da0a266f8d60a37d95211
Mason Furnes <masonfurness@hotmail.com>
2020-03-13 21:52:04 +0000

    Add files via upload

---
+++ src/main/java/net/mason/api/builder/SkullItemBuilder.java
@@ -0,0 +1,93 @@
+package net.mason.api.builder;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
+import jdk.nashorn.internal.objects.annotations.Setter;
+import net.mason.impl.Example;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.lang.reflect.Field;
+
+import static java.util.UUID.randomUUID;
+import static org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder.encodeString;
+
+public final class SkullItemBuilder extends ItemBuilder<SkullMeta> {
+
+    /**
+     * FIELDS
+     **/
+    @NotNull
+    private final String TEXTURES_URL = "http://textures.minecraft.net/texture/";
+
+
+    /**
+     * The Constructor {@link Example} for an example
+     *
+     * @param amount the amount of the ItemStack
+     */
+    public SkullItemBuilder(int amount) {
+        super(Material.SKELETON_SKULL, amount);
+    }
+
+    /**
+     * Apply an owner to the skull.
+     *
+     * @param owner Name of the Owner to be applied.
+     * @return {@link String}
+     * @deprecated [1.12.1+]
+     */
+
+    @Setter
+    @Deprecated
+    public SkullItemBuilder setOwner(@Nullable String owner) {
+        if (owner != null) applyMeta(skullMeta -> skullMeta.setOwner(owner));
+        return this;
+    }
+
+    /**
+     * Apply an owner to the skull.
+     *
+     * @param offlinePlayer OfflinePlayer object to be set.
+     * @return {@link OfflinePlayer}
+     * @since 1.12.1
+     */
+    @Setter
+    public SkullItemBuilder setOwningPlayer(@Nullable OfflinePlayer offlinePlayer) {
+        if (offlinePlayer != null) applyMeta(skullMeta -> skullMeta.setOwningPlayer(offlinePlayer));
+        return this;
+    }
+
+    /**
+     * Apply a texture to the skull.
+     *
+     * @param texture Texture to be applied.
+     * @return {@link String}
+     */
+    @Setter
+    public SkullItemBuilder applyTexture(@Nullable String texture) {
+        if (texture != null) applyMeta(skullMeta -> {
+            final GameProfile gameProfile = new GameProfile(randomUUID(), null);
+            final String encodedTexture = encodeString(String.format(
+                    "{textures:{SKIN:{url:\"%s%s\"}}}",
+                    TEXTURES_URL,
+                    texture
+            ));
+
+            final Property property = new Property("textures", encodedTexture);
+            gameProfile.getProperties().put("textures", property);
+
+            try {
+                final Field field = skullMeta.getClass().getDeclaredField("profile");
+                field.setAccessible(true);
+                field.set(skullMeta, gameProfile);
+            } catch (Exception ex) {
+                System.out.println("Failed to set Game Profile.");
+            }
+        });
+        return this;
+    }
+}
